84766452959f69028277d4c22e41e7211bef63ed
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-01-03 12:44:08 +0900

    show comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -22,6 +22,8 @@ class Article < ApplicationRecord
   validates :content, length: { minimum: 10 }
   validates :content, uniqueness: true
 
+  has_many :comments, dependent: :destroy
+
   belongs_to :user
 
   validate :validate_title_and_content_length
