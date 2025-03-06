0feb91ac63acf19b7ce78538b4d66ba525d39e9b
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-03-06 19:21:49 +0900

    after_createでメール送付

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -22,6 +22,14 @@ class Relationship < ApplicationRecord
   belongs_to :follower, class_name: 'User'
   belongs_to :following, class_name: 'User'
 
+
+  after_create :send_email
+
+  private
+  def send_email
+    RelationshipMailer.new_follower(following, follower).deliver_now
+  end
+
   # validates :follower_id, uniqueness: { scope: :following_id }
   # validate :disallow_self_reference
 
